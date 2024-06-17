ecb3a5f37b0502c95cd1c4b5ed8ee9a23f867cee
rsbassi9 <rajvir.bassi95@gmail.com>
2024-06-17 04:42:56 -0700

    changed file layout

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,8 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
+import App from "./App";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-  <ThirdwebProvider desiredChainId={ChainId.Sepolia}></ThirdwebProvider>
+  <ThirdwebProvider desiredChainId={ChainId.Sepolia}>
+    <Router>
+      <App />
+    </Router>
+  </ThirdwebProvider>
 );
