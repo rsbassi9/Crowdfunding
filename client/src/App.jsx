4f01615d905e9b415ccf0e773c16e5d1c6b7f144
rsbassi9 <rajvir.bassi95@gmail.com>
2024-06-17 05:33:34 -0700

    fixed syntax issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return;
+  <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
+    App
+  </div>;
 };
 
 export default App;
