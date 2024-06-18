4ce64380748e7a0f8b98ad173b2c9830823a5bb8
rsbassi9 <rajvir.bassi95@gmail.com>
2024-06-18 06:02:04 -0700

    attempting to fix loalhost blank page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import { Route, Routes } from "react-router-dom";
 const App = () => {
   return;
   <div className="relative sm:-8 p-4 bg-[#13131a] min-h-screen flex flex-row">
-    App
+    <div className="sm:flex hidden mr-10 relative">Sidebar</div>
   </div>;
 };
 
